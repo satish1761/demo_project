cfc5af9ebde0b4b74815afb82ad76199dd539bd8
satish <mudirajsatish5@gmail.com>
2021-07-10 00:49:42 +0530

    test cases

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,27 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	
+	public static Logger logger =LoggerFactory.getLogger(DemoApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("application started...............");
+	}
 
 	public static void main(String[] args) {
+		
+		
+		logger.info("application executed...............");
 		SpringApplication.run(DemoApplication.class, args);
 		
 		System.out.println("hello world");
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	public static Logger logger =LoggerFactory.getLogger(DemoApplicationTests.class);
+	
 	@Test
 	void contextLoads() {
+		logger.info("test case is executing ............");
+		assertEquals(true,true);
 	}
 
 }
