8f2b3a6b0e12f00cb1630ca5404fa5e1764de72c
satish <mudirajsatish5@gmail.com>
2021-07-10 23:42:20 +0530

    welcome message

---
+++ src/main/java/com/example/demo/controllers/Controllers.java
@@ -0,0 +1,19 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Controllers {
+	
+	@RequestMapping("/")
+	ModelAndView home() {
+		
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("home.jsp");
+		
+		return mv;
+	}
+
+}
