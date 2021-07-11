9424a5c7ed0c10e84698f5e54bfed07bbf92e61b
satish <mudirajsatish5@gmail.com>
2021-07-11 15:41:49 +0530

    error corrected

--- src/main/java/com/example/demo/controllers/Controllers.java
+++ src/main/java/com/example/demo/controllers/Controllers.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class Controllers {
 	
 	@RequestMapping("/")
-	ModelAndVie home() {
+	ModelAndView home() {
 		
 		ModelAndView mv=new ModelAndView();
 		mv.setViewName("home.jsp");
